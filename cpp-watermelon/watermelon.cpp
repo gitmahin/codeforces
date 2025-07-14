6435b6891d5538bb53a9f164ecc68ea8212afc42
gitmahin <nimahin25@gmail.com>
2025-07-15 00:22:53 +0600

    update comment

--- cpp-watermelon/watermelon.cpp
+++ cpp-watermelon/watermelon.cpp
@@ -3,11 +3,16 @@ using namespace std;
 
 int main()
 {
+    // watermelon weight
     int w;
     cin >> w;
 
+    // input condition 1 ≤ n ≤ 100
     if (w >= 1 && w <= 100)
     {
+        // if weight is divisible and greater than 2 -> cause we need even not odd.
+        // 2/2 = 1+1 , 1 is odd
+        // 4/2 = 2+2 , 2 is even
         if (w % 2 == 0 && w > 2)
         {
             cout << "YES" << endl;
--- cpp-way-too-long-words/way-too-long-words.cpp
+++ cpp-way-too-long-words/way-too-long-words.cpp
@@ -2,14 +2,16 @@
 using namespace std;
 
 int main() {
+
     // number of words
     int n;
     string word;
     cin>>n;
-    // 1 ≤ n ≤ 100
+
     for (int i = 0; i < n; i++){
         cin>>word;
         int word_length = word.length();
+        // input condition 1 ≤ n ≤ 100
         ( n >= 1 && n <= 100 ) && (( word_length > 10 ) ? cout<<word[0]<<(word_length-2)<<word[(word_length-1)]<<endl : cout<<word<<endl); 
     }
     return 0;
