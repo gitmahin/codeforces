2397e180523f53719b0218c885a45b54a204176d
gitmahin <nimahin25@gmail.com>
2025-07-15 00:13:52 +0600

    added

---
+++ cpp-way-too-long-words/way-too-long-words.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // number of words
+    int n;
+    string word;
+    cin>>n;
+    // 1 ≤ n ≤ 100
+    for (int i = 0; i < n; i++){
+        cin>>word;
+        int word_length = word.length();
+        ( n >= 1 && n <= 100 ) && (( word_length > 10 ) ? cout<<word[0]<<(word_length-2)<<word[(word_length-1)]<<endl : cout<<word<<endl); 
+    }
+    return 0;
+    
+}
